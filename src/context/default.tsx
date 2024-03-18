4277276321334ce92c4ba436d6de081b6baa0edf
flick <github@0xflick.xyz>
2024-03-18 13:57:05 -0600

    add user attributes

--- src/context/default.tsx
+++ src/context/default.tsx
@@ -1,4 +1,5 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { i18n as I18nType } from "i18next";
@@ -6,6 +7,21 @@ import flsTheme from "@/theme";
 import { Web3Provider } from "./Wagmi";
 import { NotificationsProvider } from "@/features/notifications/Context";
 import { Notifications } from "@/features/notifications/Notifications";
+import { useAccount, useEnsName } from "wagmi";
+
+const Config: FC<PropsWithChildren> = ({ children }) => {
+  const { address, chain } = useAccount();
+  const { data: ensName } = useEnsName({ address });
+
+  useEffect(() => {
+    if (address) {
+      Sentry.setUser({ id: address, chain: chain?.name, ens: ensName });
+    } else {
+      Sentry.setUser(null);
+    }
+  }, [address, chain?.name, ensName]);
+  return <>{children}</>;
+};
 
 export const DefaultProvider: FC<PropsWithChildren<{ i18n?: I18nType }>> = ({
   children,
@@ -15,7 +31,7 @@ export const DefaultProvider: FC<PropsWithChildren<{ i18n?: I18nType }>> = ({
     <CssBaseline />
     <Web3Provider>
       <NotificationsProvider>
-        {children}
+        <Config>{children}</Config>
         <Notifications />
       </NotificationsProvider>
     </Web3Provider>
