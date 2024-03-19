bbbaeb95bb6eecf39c9bfbc0e08f6665b5a24b63
flick <github@0xflick.xyz>
2024-03-19 08:28:16 -0600

    missing routes

---
+++ src/app/token/[tokenId]/page.tsx
@@ -0,0 +1,53 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import {
+  client as mainnetClient,
+  flsTokenAddress as mainnetFlsTokenAddress,
+} from "@/viem/mainnet-client";
+import { erc721Abi } from "viem";
+import { IMetadata } from "@/utils/metadata";
+import { fetchJson } from "@/ipfs/client";
+import Token from "@/routes/Token";
+
+interface Props {
+  params: { tokenId: string };
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const { tokenId } = params;
+  const network = "mainnet";
+
+  return {
+    title: "Fame Lady Society",
+    description: "It's a wrap!",
+    openGraph: {
+      images: [`/${network}/og/token/${tokenId}`],
+      url: `/${network}/token/${tokenId}`,
+    },
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const network = "mainnet";
+  const { tokenId } = params;
+
+  const [metadata] = await Promise.all([
+    mainnetClient
+      .readContract({
+        abi: erc721Abi,
+        address: mainnetFlsTokenAddress,
+        functionName: "tokenURI",
+        args: [BigInt(tokenId)],
+      })
+      .then(async (tokenUri) => {
+        const metadata = await fetchJson<IMetadata>({
+          cid: tokenUri.replace("ipfs://", ""),
+        });
+        return metadata;
+      }),
+  ]);
+  return <Token tokenId={Number(tokenId)} metadata={metadata} />;
+}
