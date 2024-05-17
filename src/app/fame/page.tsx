855d30b00ce22a1ee7f06df87ef70fe0962bbdf7
flick <github@0xflick.xyz>
2024-05-17 01:59:12 -0600

    more metadat

--- src/app/fame/page.tsx
+++ src/app/fame/page.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import Fame from "@/routes/Fame";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.fameladysociety.com"),
   title: "$FAME",
   description: "The home of $FAME.",
+  openGraph: {
+    images: ["/images/fame/fame.png"],
+  },
 };
 
 export default async function Page({}: {}) {
