59f5a17a5a58e03fc4e18167bf91f8fda877dd86
flick <github@0xflick.xyz>
2024-07-25 13:56:04 -0600

    support .json

--- src/app/fame/metadata/[tokenId]/route.ts
+++ src/app/fame/metadata/[tokenId]/route.ts
@@ -6,10 +6,15 @@ interface Params {
 }
 
 export async function GET(req: NextRequest, { params }: { params: Params }) {
-  const { tokenId } = params;
+  let { tokenId } = params;
+  // check if tokenId ends in ".json"
+  if (tokenId.endsWith(".json")) {
+    // remove ".json" from the end of the tokenId
+    tokenId = tokenId.slice(0, -5);
+  }
   return NextResponse.json<IMetadata>({
     id: Number(tokenId),
-    name: `Society #${tokenId}`,
+    name: `FAME Society`,
     image: `${process.env.OG_BASE_URL}/images/fame/gold-leaf-square.png`,
     description: `This Society NFT is also one million $FAME. 
 
