1f9e0b50d049412d476ef0389eadee0e9861b4f3
flick <github@0xflick.xyz>
2024-09-01 18:13:33 -0600

    fix server side siwe

--- src/utils/siweServer.ts
+++ src/utils/siweServer.ts
@@ -2,7 +2,7 @@ import { configureServerSideSIWE } from "connectkit-next-siwe";
 
 export const siweServer = configureServerSideSIWE({
   session: {
-    cookieName: "connectkit-next-siwe",
+    cookieName: "siwe",
     password: process.env.SESSION_SECRET,
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
