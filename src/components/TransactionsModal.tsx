3c9f37d9962742880a715148ef4b5f3a9d161003
flick <github@0xflick.xyz>
2025-01-12 15:01:03 -0700

    fix: lint

--- src/components/TransactionsModal.tsx
+++ src/components/TransactionsModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback } from "react";
+import { FC, ReactNode, useCallback } from "react";
 import Dialog from "@mui/material/Dialog";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
