1e4869410f0875dde199c0e99a413138e00fd72f
flick <github@0xflick.xyz>
2025-01-16 00:21:22 -0700

    feat: jilly metadata

--- src/app/~/jilly/lingerie-dreams/layout.tsx
+++ src/app/~/jilly/lingerie-dreams/layout.tsx
@@ -1,5 +1,29 @@
 import { DefaultProvider } from "@/context/default";
 import { AppMain } from "@/layouts/AppMain";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.OG_BASE_URL!),
+  title: "Lingerie Dreams",
+  description: "A 1/1 limited edition Music NFT",
+  openGraph: {
+    images: [
+      "https://www.fameladysociety.com/~/jilly/lingerie-dreams/social.jpeg",
+    ],
+    siteName: "Lingerie Dreams",
+    title: "Lingerie Dreams",
+    description: "A 1/1 limited edition Music NFT",
+  },
+  other: {
+    ["twitter:title"]: "Fame Lady Society",
+    ["twitter:description"]: "FAQ",
+    ["twitter:image"]:
+      "https://www.fameladysociety.com/~/jilly/lingerie-dreams/social.jpeg",
+    ["twitter:card"]: "summary_large_image",
+    ["twitter:site"]: "@JillyRappaport",
+    ["twitter:creator"]: "@JillyRappaport",
+  },
+};
 
 export default async function Home({
   children,
