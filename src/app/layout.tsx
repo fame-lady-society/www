aa79cfc5b0a3c3be8f055dfc3b05f2976237ce99
flick <github@0xflick.xyz>
2025-05-02 14:44:40 -0600

    fix: revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
