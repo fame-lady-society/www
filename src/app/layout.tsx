4fad50c14fb6b72b880f8beeb114bb0ce9f51aaf
flick <github@0xflick.xyz>
2025-11-15 13:48:31 -0700

    feat: miniapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { type Viewport } from "next";
 import "@/styles/tailwind.css";
+import { useEffect } from "react";
 
 export const viewport: Viewport = {
   initialScale: 1.0,
@@ -11,6 +13,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    import("@farcaster/miniapp-sdk").then(({ sdk }) => {
+      sdk.actions.ready();
+    });
+  }, []);
   return (
     <html lang="en">
       <body>{children}</body>
