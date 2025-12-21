777889b1dc9b124f0ec9deee0f4d105a877b16a3
flick <github@0xflick.xyz>
2025-12-20 22:46:21 -0700

    fix: html body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,5 +20,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
